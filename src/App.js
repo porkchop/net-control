3a1ec1ce88dd4783aae5a720cf32290fb183ddc8
Aaron Caswell <aaron@captureplay.com>
2023-06-14 18:22:58 -0700

    add lists to track users in the phases of the net

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ function App() {
   const [users, setUsers] = useState(() => JSON.parse(localStorage.getItem('users')) || []);
   const [search, setSearch] = useState('');
   const [form, setForm] = useState({ callSign: '', name: '', location: '', notes: '' });
+  const [lists, setLists] = useState(() => JSON.parse(localStorage.getItem('lists')) || []);
+  const [currentList, setCurrentList] = useState('');
+  const [newList, setNewList] = useState('');
 
   useEffect(() => {
       localStorage.setItem('users', JSON.stringify(users));
   }, [users]);
+
+  useEffect(() => {
+      localStorage.setItem('lists', JSON.stringify(lists));
+  }, [lists]);
 
   const searchUser = (callSign) => {
       const user = users.find((user) => user.callSign.toLowerCase().startsWith(callSign.toLowerCase()));
@@ -45,6 +52,39 @@ function App() {
       setForm({ callSign: '', name: '', location: '', notes: '' });
   };
 
+  const handleListChange = (event) => {
+      setCurrentList(event.target.value);
+  };
+
+  const handleAddToList = (event) => {
+      event.preventDefault();
+      const newLists = { ...lists };
+      newLists[currentList] = newLists[currentList] || [];
+      newLists[currentList].push(form);
+      setLists(newLists);
+  };
+
+  const handleNewListChange = (event) => {
+      setNewList(event.target.value);
+  };
+
+  const handleNewList = (event) => {
+      event.preventDefault();
+      const newLists = { ...lists };
+      newLists[newList] = [];
+      setLists(newLists);
+      setCurrentList(newList);
+      setNewList('');
+  };
+
+  const handleDeleteList = (event) => {
+      event.preventDefault();
+      const newLists = { ...lists };
+      delete newLists[currentList];
+      setLists(newLists);
+      setCurrentList('');
+  };
+
   return (
       <div>
           <input type="text" value={search} onChange={handleSearch} placeholder="Search by call sign..." />
@@ -55,7 +95,28 @@ function App() {
               <input name="notes" value={form.notes} onChange={handleChange} placeholder="Notes" />
               <button type="submit">Add / Update User</button>
               <button type="button" onClick={handleDelete}>Delete User</button>
+              <button type="button" onClick={handleAddToList} disabled={!currentList}>Add to List</button>
           </form>
+          <div>
+              <select value={currentList} onChange={handleListChange}>
+                  <option value="">Select a list...</option>
+                  {Object.keys(lists).map((list) => (
+                      <option key={list} value={list}>{list}</option>
+                  ))}
+              </select>
+              <button type="button" onClick={handleDeleteList} disabled={!currentList}>Delete List</button>
+              <form onSubmit={handleNewList}>
+                  <input type="text" value={newList} onChange={handleNewListChange} placeholder="New list name" />
+                  <button type="submit">Create List</button>
+              </form>
+              {lists[currentList] && (
+                  <ul>
+                      {lists[currentList].map((user, index) => (
+                          <li key={index}>{user.callSign} - {user.name} - {user.location}</li>
+                      ))}
+                  </ul>
+              )}
+          </div>
       </div>
   );
 }
